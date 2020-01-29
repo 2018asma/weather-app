f535e830c5e959cc10debc381deb2b18136a0ecf
asma ameen alshanqiti <asmaa.alshanqiti@gmail.com>
2020-01-30 00:15:28 +0300

    Updates

---
+++ precache-manifest.8841e6b121d5f7a882b03c47239d2f89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7caf8f91532a90f3f0b2dca45901d83c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "967781fb7d5e0f9651e1",
+    "url": "/weather-app/static/js/2.890ab7e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.890ab7e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "572a9c85fad00203d427",
+    "url": "/weather-app/static/js/main.4e5f6d40.chunk.js"
+  },
+  {
+    "revision": "efc0433e71a44b7fb9f8",
+    "url": "/weather-app/static/js/runtime-main.ff53a29c.js"
+  }
+]);
